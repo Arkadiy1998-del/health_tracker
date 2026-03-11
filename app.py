cd94a9fd361fb6891a3c7500f1b6fc0bfa3cd2e7
Arkadiy1998-del <factorial65@gmail.com>
2026-03-11 13:14:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,11 +19,10 @@ def get_key(key):
 #python -m streamlit run app.py
 
 images = [
-   "https://raw.githubusercontent.com/Arkadiy1998-del/health_tracker/main/Images/5376x3072_1721973_%5Bwww.ArtFile.ru%5D.jpg",
-   "https://raw.githubusercontent.com/Arkadiy1998-del/health_tracker/main/Images/AA1M06Xa.jfif",
-   "https://raw.githubusercontent.com/Arkadiy1998-del/health_tracker/main/Images/avtor-naarok0fkor-kotiata-milye-boke.webp",
-   "https://raw.githubusercontent.com/Arkadiy1998-del/health_tracker/main/Images/ii-art-neiroset-sobaka-shchenok-mordashka-vzgliad-poza-ts-16.webp",
-   "https://raw.githubusercontent.com/Arkadiy1998-del/health_tracker/main/Images/maxresdefault.jpg"
+   "https://raw.githubusercontent.com/Arkadiy1998-del/health_tracker/main/Images/maxresdefault.jpg",
+   "https://raw.githubusercontent.com/Arkadiy1998-del/health_tracker/main/Images/5376x3072_1721973_[www.ArtFile.ru].jpg",
+   "https://raw.githubusercontent.com/Arkadiy1998-del/health_tracker/main/Images/corgi-windows-theme-22-hd.jpg",
+   "https://raw.githubusercontent.com/Arkadiy1998-del/health_tracker/main/Images/maxresdefault (1).jpg"
 ]
 
 bg_image = random.choice(images)
@@ -71,5 +70,6 @@ if st.button("Сохранить"):
     st.write("Данные отправлены!")
 
     
+
 
 
