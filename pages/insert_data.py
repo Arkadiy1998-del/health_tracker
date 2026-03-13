eb6543f3e7dae3bda2ab5ec2c21db217769f4012
Arkadiy1998-del <factorial65@gmail.com>
2026-03-13 15:59:33 +0300

    Add files via upload

---
+++ pages/insert_data.py
@@ -0,0 +1,73 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
+import psycopg2
+import os
+from dotenv import load_dotenv
+import streamlit as st
+from datetime import datetime
+import random
+
+load_dotenv()
+
+def get_key(key):
+    try:
+        return st.secrets[key]
+    except Exception:
+        return os.getenv(key)
+
+#python -m streamlit run app.py
+
+images = [
+   "https://raw.githubusercontent.com/Arkadiy1998-del/health_tracker/main/Images/5376x3072_1721973_%5Bwww.ArtFile.ru%5D.jpg",
+   "https://raw.githubusercontent.com/Arkadiy1998-del/health_tracker/main/Images/Images/AA1M06Xa.jfif",
+   "https://raw.githubusercontent.com/Arkadiy1998-del/health_tracker/main/Images/Images/avtor-naarok0fkor-kotiata-milye-boke.webp",
+   "https://raw.githubusercontent.com/Arkadiy1998-del/health_tracker/main/Images/Images/ii-art-neiroset-sobaka-shchenok-mordashka-vzgliad-poza-ts-16.webp",
+   "https://raw.githubusercontent.com/Arkadiy1998-del/health_tracker/main/Images/Images/maxresdefault.jpg"
+]
+
+bg_image = random.choice(images)
+
+st.markdown(
+    f'<style>body{{background-image:url("{bg_image}"); background-size:cover;}}</style>',
+    unsafe_allow_html=True)
+
+@st.cache_resource
+def connect():
+    engine = create_engine(
+     "postgresql+psycopg2://",
+       creator = lambda : psycopg2.connect(
+    user = get_key("DB_USERNAME"),
+    password = get_key("DB_PASSWORD"),
+    host = get_key("DB_HOST"),
+    port = get_key("DB_PORT"),
+    dbname = get_key("DB_NAME")
+    ),
+    poolclass = NullPool
+)
+    return engine
+
+
+weight = st.number_input("Вес")
+calories = st.number_input("Калории")
+cycle_day = st.selectbox("День цикла", [1, 2, 3, 4, 5])
+mood = st.slider("Настроение", 0, 10)
+if st.button("Сохранить"):
+    engine = connect()
+    temp = pd.DataFrame({
+        'date' : [datetime.now()],
+        'weight' : [weight],
+        'calories' : [calories], 
+        'cycle_day' : [cycle_day], 
+        'mood' : [mood]})
+    temp.to_sql(
+        'streamlit_raw_data',
+        con = engine,
+        schema = 'data_lake',
+        if_exists = 'append',
+        index = False,
+        method = None,
+    )
+    st.write("Данные отправлены!")
+
+    
