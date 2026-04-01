9eb23bb10a4d332323cc678696876c388405d178
Arkadiy1998-del <factorial65@gmail.com>
2026-04-01 10:48:10 +0300

    Add files via upload

---
+++ pages/help.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+# фон
+bg_image = "https://raw.githubusercontent.com/Arkadiy1998-del/health_tracker/main/Images/IMG_20260316_114430_151.jpg"
+
+# дизайн
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url("{bg_image}");
+        background-size: cover;
+        background-attachment: fixed;
+        background-position: center;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.write("Привет")
